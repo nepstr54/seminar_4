8c482f425de9a8a2467d08a6396765d105d678a9
Alex Gryanko <116000789+nepstr54@users.noreply.github.com>
2023-03-25 22:39:50 +0300

    Add files via upload

---
+++ invertedListOutput/ListNode.java
@@ -0,0 +1,9 @@
+package hw.seminar_4.invertedListOutput;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
